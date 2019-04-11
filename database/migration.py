f5d8047f5e0dc46898f8e7df4136d644b00a949a
caasi8 <isaac.houngue@imsp-uac.org>
2019-04-11 01:57:48 +0100

    Adding of migration file content

---
+++ database/migration.py
@@ -0,0 +1,42 @@
+import json, tempfile
+from pymongo import MongoClient
+
+try:
+    # create a client objet to access to database;
+    dbClient = client = MongoClient('localhost', 27017)
+    # open a connection to the database trocdb
+    db = dbClient["trocdb"]
+    # access or create a collection named dataset
+    collection = db["dataset"]
+    # drop collection if it exists
+    collection.drop()
+    
+except:
+    print("dbMessage: Connection failed!")
+else:
+    print("dbMessage: Connection succes!")
+    try:
+        # load file dataset into 
+        with open("dataset.json") as jsObject:
+            mongoDocument = json.load(jsObject)
+        collection.insert_many(mongoDocument)
+
+    except:
+        print("dbMessage: dataset.json file not found")
+    else:
+        print("dbMessage: dataset.json file load succesfully!")
+
+
+
+# use to print collection content
+# for x in collection.find():
+    # print(x) 
+
+
+
+
+
+
+
+
+
